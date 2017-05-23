c0f3525b1258dbd7cb9d801c2610633005623c13
Noah Ezekwugo <nzkwgo@uw.edu>
2017-05-22 17:44:51 -0700

    added activity shells

---
+++ Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/LeaderboardActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.nzkwgo.ballo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
---
+++ Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/StatsActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.nzkwgo.ballo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
