65526614cf5138a379c0c7662e3fd02f634dfd6d
song <eysong@uw.edu>
2017-05-25 00:07:08 -0700

    hold on

--- Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/StatsActivity.java
+++ Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/StatsActivity.java
@@ -2,12 +2,31 @@ package edu.uw.nzkwgo.ballo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatsActivity extends AppCompatActivity {
+    private TextView age;
+    private TextView bounce;
+    private TextView distance;
+    private TextView fed;
+    private TextView highestHealth;
+    private TextView highestStrength;
+    private TextView lowestHealth;
+    private TextView lowestStrength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        // Get Ui Views
+        age = (TextView) findViewById(R.id.stats_age);
+        bounce = (TextView) findViewById(R.id.stats_bounce);
+        distance = (TextView) findViewById(R.id.stats_distance);
+        fed = (TextView) findViewById(R.id.stats_fed);
+        highestHealth = (TextView) findViewById(R.id.stats_highest_health);
+        highestStrength = (TextView) findViewById(R.id.stats_highest_strength);
+        lowestHealth = (TextView) findViewById(R.id.stats_lowest_health);
+        lowestStrength = (TextView) findViewById(R.id.stats_lowest_strength);
     }
 }
