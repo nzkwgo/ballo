3abd6ea885993206595094191c7ca7f59c9747cd
song <eysong@uw.edu>
2017-06-01 00:18:42 -0700

    finished settings

--- Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/SettingActivity.java
+++ Ballo/app/src/main/java/edu/uw/nzkwgo/ballo/SettingActivity.java
@@ -1,9 +1,11 @@
 package edu.uw.nzkwgo.ballo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -15,13 +17,11 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //action bar "back"
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
         //the FM that moves fragments around
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingFragment())
                 .commit();
+
+
     }
 }
